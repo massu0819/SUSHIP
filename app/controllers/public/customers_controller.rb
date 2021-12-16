bc48f9f7e5be79bb8c436b077e0670175ad167bf
massu0819 <massu4869@gmail.com>
2021-12-16 12:21:59 +0000

    マイページ・ユーザー一覧・プロフィールエディットview

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,25 @@
 class Public::CustomersController < ApplicationController
+
+  def show
+    @customer = Customer.find(params[:id])
+    @posts = customer.posts
+  end
+
+  def index
+    @customers = Customers.all
+  end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+    if @customer.id == current_customer.id
+    else
+       redirect_to customer_path(current_customer.id)
+    end
+  end
+
+  def customer_params
+    params.require(:customer).permit(:nickname, :profile_image, :introduction)
+  end
+
+
 end
